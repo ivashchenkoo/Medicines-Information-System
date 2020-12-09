0737e2848b3425cc83e6e01a2fdbc7895d7a9909
ivashchenkoo <alexander.ivashchenkoo@gmail.com>
2020-12-10 01:17:36 +0200

    Add project files.

---
+++ Medicines Information System/Data/Models/Medicines.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Medicines_Information_System.Data.Models
+{
+    public class Medicines
+    {
+    }
+}
